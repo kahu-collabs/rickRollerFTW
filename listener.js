36f7f89d79c659f3f77aaf0334a289ac06ec07bd
Piet Geursen <pietgeursen@gmail.com>
2015-09-25 13:40:31 +1200

    Got rx stream going

--- listener.js
+++ listener.js
@@ -3,8 +3,11 @@ var FirebaseRx = require('firebase-rx');
 var source = new FirebaseRx('https://sweltering-inferno-8171.firebaseio.com/kahu')
   .observe('child_added')
 
-source.subscribe(function(x){
-  console.log(x)
+var playCommands = source.filter(function(command){
+  return command.command == 'play'
+})
 
+playCommands.subscribe(function(x){
+  console.log(x)
 })
 
